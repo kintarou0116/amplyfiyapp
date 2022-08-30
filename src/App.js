6c68b7f76882f8b466f4a2e547f5a237d306a3fb
kintarou Ninomiya <kintarou0116sheen@gmail.com>
2022-08-30 20:48:57 +0900

    added auth-02

--- src/App.js
+++ src/App.js
@@ -1,20 +1,38 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
 import { Amplify } from 'aws-amplify';
-
-import { Authenticator } from '@aws-amplify/ui-react';
+import { withAuthenticator } from '@aws-amplify/ui-react';
 import '@aws-amplify/ui-react/styles.css';
-
 import awsExports from './aws-exports';
 Amplify.configure(awsExports);
 
-export default function App() {
+function App({ isPassedToWithAuthenticator, signOut, user }) {
+  isPassedToWithAuthenticator = true; // getStaticProps動いてない
+  if (!isPassedToWithAuthenticator) {
+    throw new Error(`isPassedToWithAuthenticator was not provided`);
+  }
+
   return (
-    <Authenticator>
-      {({ signOut, user }) => (
-        <main>
-          <h1>Hello {user.username}</h1>
-          <button onClick={signOut}>Sign out</button>
-        </main>
-      )}
-    </Authenticator>
+    <div className="App">
+      <header>
+        <img src={logo} className="App-logo" alt="logo" />
+        <h1>We now have Auth!</h1>
+      </header>
+      <>
+        <h1>Hello {user.username}</h1>
+        <button onClick={signOut}>Sign out</button>
+      </>
+    </div>
   );
+}
+
+export default withAuthenticator(App);
+
+export async function getStaticProps() {
+  return {
+    props: {
+      isPassedToWithAuthenticator: true,
+    },
+  };
 }
